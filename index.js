facbca90f0ca468366fbe1e6a93ae2d48261ed2c
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-28 22:01:53 +0200

    created an api

--- index.js
+++ index.js
@@ -32,12 +32,25 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+
+		const database = client.db('bistroDB');
+		const menuCollection = database.collection('menu');
+		const reviewsCollection = database.collection('reviews');
+		app.get('/menu', async (req, res) => {
+			const result = await menuCollection.find().toArray();
+			res.send(result);
+		});
+		app.get('/reviews', async (req, res) => {
+			const result = await reviewsCollection.find().toArray();
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log('Pinged your deployment. You successfully connected to MongoDB!');
 	} finally {
 		// Ensures that the client will close when you finish/error
-		await client.close();
+		// await client.close();
 	}
 }
 run().catch(console.dir);
